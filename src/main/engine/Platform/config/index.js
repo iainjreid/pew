afca8444c7ca331d63af350d709b201072823410
Iain Reid <iainreid382@gmail.com>
2019-12-26 20:31:12 +0000

    chore: Initial commit
    This is a continuation of a mini physics engine I started writing in my
    personal GitHub site repository.
    
    I'll aim to keep a link to that repository in the README for this one,
    just for reference!

---
+++ src/main/engine/Platform/config/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+export const fpsDetails = true
+
+export const fpsUpdateFrequency = 1000
+
+export const boundryBoxEnabled = false
+
+export const debugEnabled = true
+
+export const debugBox = false
---
+++ src/main/engine/index.js
@@ -0,0 +1,4 @@
+'use strict'
+
+export { Platform } from './Platform'
+export { View } from './View'
