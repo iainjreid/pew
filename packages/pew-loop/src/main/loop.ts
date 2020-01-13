65b4dccbe193b5d86fb83222d131f76edfd88af2
Iain Reid <iainreid382@gmail.com>
2020-01-13 22:41:03 +0000

    chore: Complete base loop implementation

--- packages/pew-loop/src/main/loop.ts
+++ packages/pew-loop/src/main/loop.ts
@@ -6,12 +6,42 @@ export interface LoopTask {
 
 export class Loop {
   protected tasks: LoopTask[] = [];
+  protected running: boolean = false;
 
-  constructor (
-    public name: string
-  ) { }
+  constructor(protected freq: number) {}
 
   public add(fn: () => void, priority: number, scope: any): void {
-    this.tasks.push({ fn, priority, scope});
+    for (let i = 0, n = this.tasks.length; i < n; i++) {
+      if (priority > this.tasks[i].priority) {
+        this.tasks.splice(i, 0, { fn, priority, scope });
+      }
+    }
+  }
+
+  public start(): void {
+    this.running = true;
+    this.process();
+  }
+
+  public stop(): void {
+    this.running = false;
+  }
+
+  private process(): void {
+    if (!this.running) {
+      return;
+    }
+
+    // Process Recording - start
+    const processStart = performance.now()
+
+    for (let { fn, scope } of this.tasks) {
+      fn.call(scope);
+    }
+
+    // Process Recording - end
+    const processEnd = performance.now()
+
+    setTimeout(() => this.process(), 1000 / this.freq - (processEnd - processStart))
   }
 }
