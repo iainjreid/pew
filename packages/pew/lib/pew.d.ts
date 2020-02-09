cef80f8bc17625549f43db94c859f4f354dc5109
Iain Reid <iainreid382@gmail.com>
2020-02-09 11:39:52 +0000

    chore: Remove old code from root library

---
+++ packages/pew/lib/pew.d.ts
@@ -0,0 +1,5 @@
+export * from "@chaffity/pew-canvas";
+export * from "@chaffity/pew-input";
+export * from "@chaffity/pew-loop";
+export * from "@chaffity/pew-objects";
+export * from "@chaffity/pew-utils";
---
+++ packages/pew/lib/pew.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+  ...require("@chaffity/pew-canvas"),
+  ...require("@chaffity/pew-input"),
+  ...require("@chaffity/pew-loop"),
+  ...require("@chaffity/pew-objects"),
+  ...require("@chaffity/pew-utils"),
+}
